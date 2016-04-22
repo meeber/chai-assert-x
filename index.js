d574c94ff8f541b26104b264c606b87845459c1b
meeber <meeber@gametypething.com>
2016-04-22 17:45:51 -0400

    Pull index.js from build to root

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var semver = require("semver");
 
 if (semver.gte(process.version, "v6.0.0-rc.0"))
-  module.exports = require("./current");
+  module.exports = require("./build/current");
 else if (typeof Reflect === "object")
-  module.exports = require("./legacy");
+  module.exports = require("./build/legacy");
 else
-  module.exports = require("./legacy-shim");
+  module.exports = require("./build/legacy-shim");
